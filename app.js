279b0cde7e9dc31d94ccc039d1c378fa1ecdfbe3
Soumyajit Naskar <soumyajit13102003@gmail.com>
2024-04-04 23:03:26 +0530

    Initial commit

--- app.js
+++ app.js
@@ -17,12 +17,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    origin: ["*"],
   })
 );
-
 //using routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
